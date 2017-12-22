eb386c374f5cc7eb676586985c151f12b94f5633
袁秉瑞 <bingrui.yuan@roadshare.com>
2017-12-22 16:23:46 +0800

    click img to add canvas

--- src/components/composite-image.vue
+++ src/components/composite-image.vue
@@ -3,7 +3,7 @@
     <div class="handleBtnBox">
       <el-input
         class="inputStyle"
-        placeholder="请输入文件名"
+        placeholder="请输入文件名e.g.:elevater.png"
         v-model="fileName"
         clearable>
       </el-input>
@@ -31,8 +31,8 @@
     </div>
     <div class="showImgBox">
       <div class="boxSize boxSizeT thumbnailBox">
-        <template v-for="imgone in thumbnailImg">
-          <img :src="imgone.src" :id="imgone.id" ref="images">
+        <template v-for="(imgone, index) in thumbnailImg">
+          <img :src="imgone.src" :id="imgone.id" ref="images" @click="selectedImg(index)">
         </template>
         <el-upload
           action=""
@@ -45,7 +45,7 @@
         </el-upload>
       </div>
       <div class="boxSize canvasBox">
-        <canvas ref="canvas" width="150" height="300" style="border:1px solid #d3d3d3;"></canvas>
+        <canvas ref="canvas" width="150" height="300"></canvas>
       </div>
       <div class="boxSize compositeBox">
         <a href="" download="elevater" ref="linkDown">
@@ -62,9 +62,10 @@ export default {
   data () {
     return {
       thumbnailImg: [{id: 'car', src: '../../static/VC01.png'}, {id: 'ceilling', src: '../../static/DD06A.png'}, {id: 'floor', src: '../../static/DF106.png'}, {id: 'controlBox', src: '../../static/CC102.png'}],
-      fileName: 'elevater.png',
+      fileName: '',
       dialogImageUrl: '',
-      dialogVisible: false
+      canvas: '',
+      ctx: ''
     }
   },
   mounted () {
@@ -72,29 +73,21 @@ export default {
   },
   methods: {
     draw: function() {
-      let canvas = this.$refs.canvas
-      let ctx = canvas.getContext('2d')
-      for (let i = 0; i < this.$refs.images.length; i++) {
-        if(this.$refs.images[i].id === 'car' || this.$refs.images[i].id === 'controlBox' || this.$refs.images[i].id === 'ceilling' || this.$refs.images[i].id === 'floor'){
-          ctx.drawImage(this.$refs.images[i], 0, 0, 150, 300)
-        }
-      }
+      this.canvas = this.$refs.canvas
+      this.ctx = this.canvas.getContext('2d')
     },
     saveImageInfo: function() {
       let mycanvas = this.$refs.canvas
       let image = mycanvas.toDataURL("image/png")
       this.$refs.linkDown.href = image
       this.$refs.downImg.src = image
-      // 在新的窗口打开查看
-      // let w=window.open('about:blank','image from canvas') 
-      // w.document.write("<img src='"+image+"' alt='from canvas'/>");
     },
     saveAsLocalImage: function() {
       let myCanvas = this.$refs.canvas
       let image = myCanvas.toDataURL("image/png").replace("image/png", "image/octet-stream")
       let save_link = this.$parent.$el.ownerDocument.createElementNS('http://www.w3.org/1999/xhtml', 'a')
       save_link.href = image
-      save_link.download = this.fileName
+      save_link.download = this.fileName ? this.fileName : 'elevater.png'
       let event = this.$parent.$el.ownerDocument.createEvent('MouseEvents')
       event.initMouseEvent('click',true,false,window,0,0,0,0,0,false,false,false,false,0,null)
       save_link.dispatchEvent(event)
@@ -104,9 +97,12 @@ export default {
       console.log(fileList)
     },
     handlePictureCardPreview(file) {
-      console.log(file.url)
-      this.dialogImageUrl = file.url
-      this.dialogVisible = true
+      let img = new Image()
+      img.src = file.url
+      this.ctx.drawImage(img, 0, 0, 150, 300)
+    },
+    selectedImg: function(index) {
+      this.ctx.drawImage(this.$refs.images[index], 0, 0, 150, 300)
     }
   }
 }
